563d90a04c49d75f1098fc73b7aadd68c79941c9
Farouk <wattfarouk@gmail.com>
2019-04-07 00:02:40 +0200

    javascript import error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 
 import { Component } from '@angular/core';
+/*import 'src/assets/js/jquery.min';
+import 'src/assets/js/bootstrap.min';
+import 'src/assets/js/nouislider.min';
+import 'src/assets/js/slick.min';
+import 'src/assets/js/jquery.zoom.min';
+import  'src/assets/js/main';*/
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+ styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'JeBouquine';
